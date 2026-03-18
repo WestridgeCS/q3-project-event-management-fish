d56d9d89c2704054b281436a30483078dede1b9a
Annika618 <38537ajl@gmail.com>
2026-03-18 11:16:32 -0700

    added shows

--- seedShows.js
+++ seedShows.js
@@ -31,22 +31,67 @@ iconPath: "/uploads/collegeIcons/ucla.png"
 },
 
 {
-name: "Miraculous: Tales of Ladybug and Cat Noir",
-description: "A show about the wonderful adventures of the superheros ladybug and chat noir!",
-watchTime: "55 hours",
+name: "My Little Pony",
+description: "A super awesome adveture about friendship with the most awesome group of ponies minus fluttershy",
+watchTime: "156 hours",
 watched: False,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
 
 {
-name: "Miraculous: Tales of Ladybug and Cat Noir",
-description: "A show about the wonderful adventures of the superheros ladybug and chat noir!",
-watchTime: "55 hours",
+name: "Peppa Pig",
+description: "Pigs and others doing what pigs do best!",
+watchTime: "208 hours",
 watched: False,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
+
+{
+name: "Glitter Force",
+description: "Magic girls!!!",
+watchTime: "19 hours",
+watched: False,
+comments: "",
+iconPath: "/uploads/collegeIcons/ucla.png"
+},
+
+{
+name: "PJ mask",
+description: "Kids fighting other kids",
+watchTime: "128 hours",
+watched: False,
+comments: "",
+iconPath: "/uploads/collegeIcons/ucla.png"
+},
+
+{
+name: "Super Wing",
+description: "talking planes delive packages and solves peoples external crisis",
+watchTime: "272 hours",
+watched: False,
+comments: "",
+iconPath: "/uploads/collegeIcons/ucla.png"
+},
+
+{
+name: "Pokemon the series: the beginning",
+description: "kid who never grows up abuses animals by forces them to fight",
+watchTime: "510 hours",
+watched: False,
+comments: "",
+iconPath: "/uploads/collegeIcons/ucla.png"
+},
+
+{
+name: "Avatar the Last Airbender",
+description: "Everything changed when the fire nation attacked",
+watchTime: "30 hours",
+watched: False,
+comments: "",
+iconPath: "/uploads/collegeIcons/ucla.png"
+}
 ]
 
 
