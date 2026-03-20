b07f775464b44be1838fc38045d6daed0bceb009
Yolanda Wang <mynameisyolandawang@gmail.com>
2026-03-20 10:20:42 -0700

    fixed shows data

--- seedShows.js
+++ seedShows.js
@@ -34,7 +34,7 @@ iconPath: "/uploads/showIcons/ucla.png"
 name: "My Little Pony",
 description: "A super awesome adveture about friendship with the most awesome group of ponies minus fluttershy",
 watchTime: "156 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/showIcons/ucla.png"
 },
@@ -43,7 +43,7 @@ iconPath: "/uploads/showIcons/ucla.png"
 name: "Peppa Pig",
 description: "Pigs and others doing what pigs do best!",
 watchTime: "208 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/showIcons/ucla.png"
 },
@@ -52,7 +52,7 @@ iconPath: "/uploads/showIcons/ucla.png"
 name: "Glitter Force",
 description: "Magic girls!!!",
 watchTime: "19 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
@@ -61,7 +61,7 @@ iconPath: "/uploads/collegeIcons/ucla.png"
 name: "PJ mask",
 description: "Kids fighting other kids",
 watchTime: "128 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
@@ -70,7 +70,7 @@ iconPath: "/uploads/collegeIcons/ucla.png"
 name: "Super Wing",
 description: "talking planes delive packages and solves peoples external crisis",
 watchTime: "272 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
@@ -79,7 +79,7 @@ iconPath: "/uploads/collegeIcons/ucla.png"
 name: "Pokemon the series: the beginning",
 description: "kid who never grows up abuses animals by forces them to fight",
 watchTime: "510 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 },
@@ -88,7 +88,7 @@ iconPath: "/uploads/collegeIcons/ucla.png"
 name: "Avatar the Last Airbender",
 description: "Everything changed when the fire nation attacked",
 watchTime: "30 hours",
-watched: False,
+watched: false,
 comments: "",
 iconPath: "/uploads/collegeIcons/ucla.png"
 }
